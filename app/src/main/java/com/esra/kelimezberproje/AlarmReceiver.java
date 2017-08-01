25d315165a99c71289303d6a2722e49af8491fc2
esraktrc <esra.katirci@yandex.com>
2017-08-01 22:26:11 +0300

    Initial commit

---
+++ app/src/main/java/com/esra/kelimezberproje/AlarmReceiver.java
@@ -0,0 +1,36 @@
+package com.esra.kelimezberproje;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent notificationIntent = new Intent(context, NotificationActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(NotificationActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(5   , PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+
+        Notification notification = builder.setContentTitle("Kelimezber")
+                .setContentText("Merhaba!!")
+                .setTicker("New Message Alert!")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent).build();
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(5, notification);
+
+
+    }
+}
